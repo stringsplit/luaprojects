06115f4c10fd853f850acee121de75f270d59aec
string.split <63344832+stringsplit@users.noreply.github.com>
2021-03-05 18:49:12 +0300

    Create ScriptWareHttpSpy.lua

---
+++ ScriptWareHttpSpy.lua
@@ -0,0 +1,40 @@
+--Init and greetings
+local Init_c = consolecreate or rconsolecreate;if not Init_c then warn('Exploit dont supports');end;Init_c();Init_c = nil;(consolesettitle or rconsolesettitle)('HTTP SPY by string.split#8169');(consoleprint or rconsoleprint)(
+'\n' .. [=[
+      :::    ::: ::::::::::: ::::::::::: :::::::::          ::::::::  :::::::::  :::   ::: 
+     :+:    :+:     :+:         :+:     :+:    :+:        :+:    :+: :+:    :+: :+:   :+:  
+    +:+    +:+     +:+         +:+     +:+    +:+        +:+        +:+    +:+  +:+ +:+    
+   +#++:++#++     +#+         +#+     +#++:++#+         +#++:++#++ +#++:++#+    +#++:      
+  +#+    +#+     +#+         +#+     +#+                      +#+ +#+           +#+        
+ #+#    #+#     #+#         #+#     #+#               #+#    #+# #+#           #+#         
+###    ###     ###         ###     ###                ########  ###           ###          
+]=],'red');(consoleprint or rconsoleprint)(
+'\n' .. [=[
+Greetings, to enable HTTP SPY type "HSOn",if you want to disable it, use "HSOff" (P.S Only take effect on http.request)
+]=],'green')
+
+--Greeting end
+--Variables
+local old_htreq = http.request;
+local function InputCommand()
+	local input = (consoleinput or rconsoleinput)();
+	if input == 'HSOn' then
+		(consoleprint or rconsoleprint)('Enabled \n','green');
+		http.request = newcclosure(function(t) 
+			if type(t) == 'table' then
+				(consoleprint or rconsoleprint)('HTTP REQUEST (Exploit request) to: '.. t.Url .. ', Method: '.. t.Method .. ', Body: ' .. t.Body .. '\n','red');
+				return old_htreq(t);
+			end;
+			return old_htreq(t);
+		end)
+	elseif input == 'HSOff' then
+		hookfunction(http.request,old_htreq);
+		(consoleprint or rconsoleprint)('http.request function has been restored \n','green');
+	else
+		(consoleprint or rconsoleprint)('Unknown command or command typed wrong! \n','red');
+	end;
+	InputCommand();
+end;InputCommand(); --Command input function,and calling it for init it lol >) 
+
+
+--End of script
