de0ccfd85c1759e7cabeda475ec3a75f09b05eb3
string.split <63344832+stringsplit@users.noreply.github.com>
2021-03-10 17:40:05 +0300

    Decoding ``Body`` and logging it

--- ScriptWareHttpSpy.lua
+++ ScriptWareHttpSpy.lua
@@ -1,5 +1,6 @@
 --Init and greetings
 _G.Time = tick();
+function STTB(a,b,c,d)c=c or false;d=d or 0;local e=string.rep(" ",d)if b then e=e..b.." = "end;if type(a)=="table"then e=e.."{"..(not c and"\n"or"")for f,g in pairs(a)do e=e..STTB(g,f,c,d+1)..","..(not c and"\n"or"")end;e=e..string.rep(" ",d).."}"elseif type(a)=="number"then e=e..tostring(a)elseif type(a)=="string"then e=e..string.format("%q",a)elseif type(a)=="boolean"then e=e..(a and"true"or"false")else e=e.."\"[inserializeable datatype:"..type(a).."]\""end;return e end
 if SWHTOn then (consoleclear or rconsoleclear)() end;getgenv().SWHTOn = true;local Init_c = (consolecreate or rconsolecreate);if (not Init_c) or (getexecutorname():sub(1,#tostring(getexecutorname())) ~= 'ScriptWare') then warn('Exploit dont supports'); return end;Init_c();Init_c = nil;(consolesettitle or rconsolesettitle)('HTTP SPY by string.split#8169');(consoleprint or rconsoleprint)(
 '\n' .. [=[
       :::    ::: ::::::::::: ::::::::::: :::::::::          ::::::::  :::::::::  :::   ::: 
@@ -27,7 +28,7 @@ local function InputCommand()
 		(consoleprint or rconsoleprint)('Enabled,Log file started: ' .. tostring(LogFileName) .. '\n','green');
 		http.request = newcclosure(function(t)
 			if type(t) == 'table' then
-				appendfile(LogFileName,'\n['..Count..']\n\n' .. 'Url = '.. tostring(t.Url) ..'\n' .. 'Method = '.. tostring(t.Method) ..'\n' .. 'Body = '..tostring(t.Body) ..'\n' .. '\n['..Count..' closed]\n');Count = Count + 1;
+				appendfile(LogFileName,'\n['..Count..']\n\n' .. 'Url = '.. tostring(t.Url) ..'\n' .. 'Method = '.. tostring(t.Method) ..'\n' .. 'Body = '..tostring(STTB(game:GetService("HttpService"):JSONDecode(t.Body))) ..'\n' .. '\n['..Count..' closed]\n');Count = Count + 1;
 				(consoleprint or rconsoleprint)('HTTP REQUEST (Exploit request) to: '.. tostring(t.Url) .. ', Method: '.. tostring(t.Method) .. ', Body: ' .. tostring(t.Body) .. '\n\n','red');
 				return old_htreq(t);
 			end;
